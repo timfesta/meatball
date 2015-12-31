8c6356a9e5ae849012705e48270919ed6c4e9f4e
timfesta <timothyfesta@yahoo.com>
2015-12-31 08:38:21 -0800

    changed title and added favorites

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ActiveRecord::Base
 	belongs_to :user
+	has_many :favorites, dependent: :destroy
+	has_many :users, through: :favorites
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :recipes, dependent: :destroy
+	has_many :favorites, dependent: :destroy
+	has_many :favorite_recipes, through: :favorites, source: :recipe 
 	has_secure_password
 end
