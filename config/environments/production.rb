c2f208f14f61876a4146285bc2208f6c3fd435c1
timfesta <timothyfesta@yahoo.com>
2016-03-06 16:42:22 -0800

    added pic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # allowing for background images
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
