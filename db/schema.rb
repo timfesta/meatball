7962f3cf9dd859afc21ab584b49db767e78800a9
timfesta <timothyfesta@yahoo.com>
2016-03-06 16:30:55 -0800

    fingers crossed

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230011323) do
+ActiveRecord::Schema.define(version: 20151111215043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "favorites", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
